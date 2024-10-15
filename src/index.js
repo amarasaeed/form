65596a4fe59647d528367ab1fac63066ecb73c73
amarasaeed <amarasaeed787@gmail.com>
2024-10-15 20:26:07 +0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Form from './form/Form ';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './Ap/App'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <Form/>
-  </React.StrictMode>
-);
-
+    <App />
+  </React.StrictMode>,
+)
