ca75c6beb916ffc7ea0538e39bf3bc23397b3ff6
amarasaeed <amarasaeed787@gmail.com>
2024-10-18 13:22:44 +0500

    comit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Ap/App'
+import Form from './form/Form '
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Form/>
+  </React.StrictMode>
 )
